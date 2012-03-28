2e35c791896c36e0b8a81f69ca7c28c73f18d4bb
Justin Ellenbecker <strifejester@gmail.com>
2012-03-27 19:39:51 -0500

    Added js files for ROM Manager manual hosting

---
+++ manifest.js
@@ -0,0 +1,10 @@
+{
+    "developer": "StrifeJester",
+    "free": "true",
+    "id": "StrifeJester",
+    "manifest": "http://android.strifejester.com/ClockworkStuff/romlist.js",
+    "roms": {
+        "crespo4g": "true"
+    },
+    "summary": "AOSP, BB and Some Custom work"
+}
